87d2a18d38d7243d7fa8719950dc4ec7f89537d8
Pablo_Herrera <pabloemi1998@gmail.com>
2024-07-24 20:25:48 -0500

    update models mongo

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -8,6 +8,7 @@ export const envs = {
 
   JWT_SEED: get('JWT_SEED').required().asString(),
 
+  SEND_EMAIL: get('SEND_EMAIL').default('false').required().asBool(),
   MAILER_SERVICE: get('MAILER_SERVICE').required().asString(),
   MAILER_EMAIL: get('MAILER_EMAIL').required().asString(),
   MAILER_SECRET_KEY: get('MAILER_SECRET_KEY').required().asString(),
